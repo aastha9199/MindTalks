fb97cc84463784b59b8b3f888b332618209bce33
Ishan Rakte <78254476+IshanRakte@users.noreply.github.com>
2023-10-01 18:34:14 +0530

    Frontend for Create and Tip Posts Added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,20 +8,20 @@ class Navbar extends Component {
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
+          href="https://github.com/IshanRakte"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Dapp University
+          MindTalks
         </a>
-
+        
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <small className="text-secondary">
               <small id="account">{this.props.account}</small>
             </small>
             { this.props.account
-              ? <img
+              ? <img 
                 className='ml-2'
                 width='30'
                 height='30'
